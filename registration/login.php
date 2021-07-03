4a158fefc901167e5e0aa937cf05680f438f61e0
SpectoRMoonBlaze <81097576+SpectoRMoonBlaze@users.noreply.github.com>
2021-07-03 13:36:28 +0200

    Add files via upload

---
+++ registration/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<style type="text/css">
+		.logform{
+
+		}
+	</style>
+</head>
+<body>
+	<form method="post">
+		<div class="form">
+			<div class="logform">
+				
+				
+			</div>
+		</div>
+	</form>
+</body>
+</html>
